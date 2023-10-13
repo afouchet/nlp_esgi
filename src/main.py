eeb4e67101a5a0a947b59aebf22e121e54ce05cc
ErwanDuprey <erwan.duprey@free.fr>
2023-10-13 17:50:07 +0200

    Part1 test

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import click
 import json
 import numpy as np
-from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn.model_selection import cross_val_score
 
 from data.make_dataset import make_dataset
 from features.make_features import make_features
@@ -39,10 +39,24 @@ def train(task, input_filename, model_dump_filename, config):
 @click.command()
 @click.option("--task", help="Can be is_comic_video, is_name or find_comic_name")
 @click.option("--input_filename", default="data/raw/train.csv", help="File training data")
-@click.option("--model_dump_filename", default="models/dump.json", help="File to dump model")
+@click.option("--model_dump_filename", default="model/dump.json", help="File to dump model")
 @click.option("--output_filename", default="data/processed/prediction.csv", help="Output file for predictions")
-def test(task, input_filename, model_dump_filename, output_filename):
-    pass
+@click.option("--config", default={}, help="Config to use")
+def test(task, input_filename, model_dump_filename, output_filename, config):
+    try:
+        config = json.loads(config)
+    except Exception:
+        print("WARNING: Argument config not well parsed.")
+        config = None
+    df = make_dataset(input_filename)
+
+    # Make features (tokenization, lowercase, stopwords, stemming...)
+    X, y, steps = make_features(df, task, config)
+
+    # Object with .fit, .predict methods
+    model = load_model(steps, model_dump_filename)
+
+    return model.named_steps["loaded_model"].dump(model_dump_filename)
 
 
 @click.command()
