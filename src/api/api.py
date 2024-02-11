485bbed4d2c33c380213434c3b26330a5764402d
Arnaud Fouchet <foucheta@gmail.com>
2024-02-11 21:32:15 +0100

    Added a the API function

---
+++ src/api/api.py
@@ -0,0 +1,6 @@
+def send_message(receiver, message):
+    return f"Sent to \"{receiver}\" the message: \"{message}\""
+
+
+def ask_RAG(question):
+    return f"Asked to RAG: \"{question}\"\nThe RAG replied: \"I don't know\""
