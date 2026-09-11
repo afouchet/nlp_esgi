2b3baded55c60a3283a10c08c537b19c1d972179
Arnaud Fouchet <foucheta@gmail.com>
2026-09-11 11:47:26 +0200

    Can use OpenRouter

--- src/llm_call.py
+++ src/llm_call.py
@@ -7,27 +7,51 @@ from pydantic import BaseModel
 
 load_dotenv()
 
-client = openai.OpenAI(
-     base_url="https://api.groq.com/openai/v1",
-     api_key=os.environ["GROQ_API_KEY"],
-)
+def main():
+    provider = os.environ["LLM_PROVIDER"]
 
-# Call classic
-reply = client.chat.completions.create(
-    messages=[{"role": "user", "content": "Who is the best NLP teacher?"}],
-    model="openai/gpt-oss-20b",
-)
-print(reply.choices[0].message.content)
+    client = get_client(provider)
+    model_name = get_model_name(provider)
+
+    print(" --- Making classic call --- ")
+    reply = client.chat.completions.create(
+        messages=[{"role": "user", "content": "Who is the best NLP teacher?"}],
+        model=model_name,
+    )
+    print(reply.choices[0].message.content)
+    
+    
+    print("")
+    print(" --- Making call with response format --- ")
+    # Call with structured output
+    class ComicName(BaseModel):
+        name: str
+    
+    reply = client.chat.completions.parse(
+        messages=[{"role": "user", "content": "Extract the comedian's name in this video title: 'Ne me parlez plus d IA - la chronique de Thomas VDB'"}],
+        # model="openai/gpt-oss-20b",
+        model=model_name,
+        response_format=ComicName,
+    )
+    
+    print(reply.choices[0].message.content)
+def get_model_name(provider):
+    return {
+        "GROQ": "openai/gpt-oss-20b",
+        "OPEN_ROUTER": "nex-agi/nex-n2.5-mini:free",
+    }[provider]
 
 
-# Call with structured output
-class ComicName(BaseModel):
-    name: str
+def get_client(provider):
+    url = {
+        "GROQ": "https://api.groq.com/openai/v1",
+        "OPEN_ROUTER": "https://openrouter.ai/api/v1",
+    }[provider]
 
-reply = client.chat.completions.parse(
-    messages=[{"role": "user", "content": "Extract the comedian's name in this video title: 'Ne me parlez plus d IA - la chronique de Thomas VDB'"}],
-    model="openai/gpt-oss-20b",
-    response_format=ComicName,
-)
+    return openai.OpenAI(
+         base_url=url,
+         api_key=os.environ[f"{provider}_API_KEY"],
+    )
 
-print(reply.choices[0].message.content)
+if __name__ == "__main__":
+    main()
